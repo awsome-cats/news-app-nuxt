54f8eac2df5239b38e1887bc9b3678f4310fdaf8
awsome-cats <cattwin82@gmail.com>
2020-05-30 11:05:11 +0900

    create navigation drawer & category path

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,46 @@
 <template>
-  <div class="md-layout md-alignment-center">
+  <div class="md-layout md-alignment-center" style="margin: 4em 0">
+    <!-- Top Navbar Start -->
+    <md-toolbar class="fixed-toolbar" elevation="1">
+      <md-button class="md-icon-button">
+        <md-icon>menu</md-icon>
+      </md-button>
+      <nuxt-link class="md-primary md-title" to="/">
+      Nuxt News
+      </nuxt-link>
+      <div class="md-toolbar-section-end">
+        <md-button @click="$router.push('/login')">Login</md-button>
+        <md-button @click="$router.push('/register')">Register</md-button>
+        <md-button @click="showSidePanel = true">
+          Category
+        </md-button>
+      </div>
+    </md-toolbar>
+    <!-- Top Navbar End-->
+    <!-- News Categories (Right Drawer) Start -->
+    <md-drawer class="md-right" md-fixed :md-active.sync="showSidePanel">
+      <md-toolbar :md-elevation="1">
+        <span class="md-title">News Categories</span>
+      </md-toolbar>
+      <!-- progress bar -->
+      <md-progress-bar v-if="loading" md-mode="indeterminate"></md-progress-bar>
+      
+      <!-- list in Drawer Start -->
+      <md-list>
+        <md-subheader class="md-primary">Categories</md-subheader>
+        <md-list-item v-for="(newsCategory, i) in newsCategories" :key="i" @click="loadCategory(newsCategory.path)">
+          <md-icon :class="newsCategory.path === category ? 'md-primary' : ''">{{newsCategory.icon}}</md-icon>
+          <span class="md-list-item-text">{{newsCategory.name}}</span>
+        </md-list-item>
+      </md-list>
+      <!-- list in Drawer End -->
+    </md-drawer>
+    <!-- News Categories (Right Drawer) End -->
+    
     <div class="md-layout-item md-size-95">
       <md-content class="md-layout md-gutter" style="background:#007998; padding: 1em;">
         <!-- v-for -->
-        <ul v-for="headline in topHeadline" :key="headline.id" class="md-layout-item md-large-size-25 md-medium-size-33 md-small-size-50 md-xsmall-size-100">
+        <ul v-for="headline in headlines" :key="headline.id" class="md-layout-item md-large-size-25 md-medium-size-33 md-small-size-50 md-xsmall-size-100">
           <md-card style="margin-top: 1em;" md-with-hover>
             <md-ripple>
             <md-card-media md-ratio="16:9">
@@ -47,13 +84,48 @@
 </template>
 
 <script>
+
 export default {
-  async asyncData({ app }) {
-    const topHeadline = await app.$axios.$get('/api/top-headlines?country=jp')
-    // console.log(topHeadline.articles)
+  data() {
     return {
-      topHeadline: topHeadline.articles
+      showSidePanel: false,
+      newsCategories: [
+        { name: 'トップヘッドライン', path: '', icon: 'today'},
+        { name: 'テクノロジー', path: 'technology', icon: 'keyboard'},
+        { name: 'ビジネス', path: 'business', icon: 'business_center'},
+        { name: 'エンターテイメント', path: 'enterTainment', icon: 'weekend'},
+        { name: 'ヘルス', path: 'health', icon: 'fastfood'},
+        { name: 'サイエンス', path: 'science', icon: 'fingerprint'},
+        { name: 'スポーツ', path: 'sports', icon: 'golf_course'}
+      ]
     }
+  },
+  methods: {
+    async loadCategory(category) {
+      this.$store.commit('setCategory', category)
+      await this.$store.dispatch('loadHeadlines',`/api/top-headlines?country=jp&category=${this.category}`)
+    }
+  },
+  computed: {
+    headlines() {
+      return this.$store.getters.headlines
+    },
+    category() {
+      return this.$store.getters.category
+    },
+    loading() {
+      return this.$store.getters.loading
+    }
+  },
+  // async asyncData({ app }) {
+  //   const topHeadline = await app.$axios.$get('/api/top-headlines?country=jp')
+  //   console.log(topHeadline)
+  //   return {
+  //     topHeadline: topHeadline.articles
+  //   }
+  // }
+  async fetch({ store }) {
+    await store.dispatch('loadHeadlines', `/api/top-headlines?country=jp&category=${store.state.category}`)
   }
 }
 </script>
@@ -61,5 +133,10 @@ export default {
 <style scoped>
   .small-icon {
     font-size: 18px !important;
+  }
+  .fixed-toolbar {
+    position: fixed;
+    top: 0;
+    z-index: 5;
   }
 </style>
